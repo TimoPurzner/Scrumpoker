e03670d887a863a71a183ca1c85b55602933619c
Timo Purzner <timo.purzner@codecamp-n.com>
2021-02-01 22:49:11 +0100

    [ADD] restyling of scrum master view

--- frontend/src/sites/scrum_master_view/scrum_master_view.tsx
+++ frontend/src/sites/scrum_master_view/scrum_master_view.tsx
@@ -4,6 +4,8 @@ import EstimationRoom from '../../model/estimation-room';
 import api from '../../api/http-client';
 import UsersTable from '../../components/users_table';
 
+import './scrum_master_view.scss'
+
 
 const route = '/scrum-master-view/:id'
 export default function ScrumMasterView() {
@@ -11,6 +13,7 @@ export default function ScrumMasterView() {
     const { id } = useParams<Record<string, string>>();
     const [story, setStory] = useState('');
     const [estimationRoom, setEtimationRoom] = useState<EstimationRoom>();
+    const tooltip = {tooltip: 'Klick to copy'};
 
     useEffect(() => {
         api(`estimation_rooms/${id}`)
@@ -22,25 +25,44 @@ export default function ScrumMasterView() {
         
     }, [id]);
 
-  return (
+    function roomIdToClickboard() {
+        var tempInput = document.createElement("input");
+        tempInput.setAttribute('style', 'position: absolute; left: -1000px; top: -1000px')
+        tempInput.setAttribute('value', estimationRoom?._id ?? id)
+        document.body.appendChild(tempInput);
+        tempInput.select();
+        document.execCommand("copy");
+        document.body.removeChild(tempInput);
+    }
+
+    return (
     <div className='scrum-master'>
-        <div className="scrum-master__code">
-            Raum beitreten: 
-            <span className='scrum-master__code__text'> {estimationRoom?._id} </span>
+        <h1 className='scrum-master__header'>
+            SCRUM-Estimation Übersicht
+        </h1>
+        <div className='scrum-master__share'>
+            Code zum teilen für Teilnehmer:
+            <span id='scrum-master-room-id' {...tooltip} className='scrum-master__share__code' onClick={roomIdToClickboard}>
+                {estimationRoom?._id}
+            </span>
         </div>
-        <div className='scrum-master__input'>
-            <textarea onChange={(event) => setStory(event.target.value)} value={story} />
+        <div className='scrum-master__story-input'>
+            <textarea contentEditable='true'
+                className='scrum-master__story-input__textarea'
+                placeholder='Beschreibe die zuschätzende Story kurz'
+                onChange={(event) => setStory(event.target.value)} 
+                value={story}
+            />
+            <button className='scrum-master__story-input__submit big'>Setze Story</button>
         </div>
         <div className='scrum-master__users'>
-            <div className='scrum-master__users_header'>
-                Schätzer:
+            <div className='scrum-master__users__header'>
+                Teilnehmer:
             </div>
-        
             <UsersTable room_id={id} />
-        
         </div>
     </div>
-  );
+    );
 }
 
 export { route }
