d2aff8b3ac976962a1236e5b2acd0fda0a214f45
Timo Purzner <timo.purzner@codecamp-n.com>
2021-03-08 18:00:36 +0100

    [FIX] button test

--- frontend/src/components/button/button.test.tsx
+++ frontend/src/components/button/button.test.tsx
@@ -1,28 +1,26 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Button from './button';
 
 describe('Button Component', () => {
 
     it('should call the callback function on click', () => {
-        const testObject = {test: ():any => {}};
-        jest.spyOn(testObject, 'test');
-        const {container} = render(<Button onClick={testObject.test}>Test</Button>);
+        const testObject = {callback: () => {}};
+        jest.spyOn(testObject, 'callback');
+        const {container} = render(<Button onClick={testObject.callback}>Test</Button>);
         const button = container.querySelector('button');
-        button?.click();
-        expect(testObject.test).toHaveBeenCalled();
+        button.click();
+        expect(testObject.callback).toHaveBeenCalled();
     });
+
     
     it('should not show loading indicator', () => {
-        const {container} = render(<Button loading={true} onClick={() => {}}>Test</Button>);
-        const button = container.querySelector('button');
+        render(<Button loading={true} onClick={() => {}}>Test</Button>);
         expect(screen.getByText(/Loading/)).toBeTruthy();
         expect(screen.queryByText(/Test/)).toBeNull();
     });
     
     it('should display children if not loading', () => {
-        const {container} = render(<Button loading={false} onClick={() => {}}>Test</Button>);
-        const button = container.querySelector('button');
+        render(<Button loading={false} onClick={() => {}}>Test</Button>);
         expect(screen.getByText(/Test/)).toBeTruthy();
     });
     
